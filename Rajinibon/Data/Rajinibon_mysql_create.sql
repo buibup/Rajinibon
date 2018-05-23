34ca3a8fca591d06a175969b94445c2cb1e0079f
buibup <b5209194@gmail.com>
2018-05-24 00:22:32 +0700

    edit sql table

--- Rajinibon/Data/Rajinibon_mysql_create.sql
+++ Rajinibon/Data/Rajinibon_mysql_create.sql
@@ -9,11 +9,9 @@ CREATE TABLE `students_check_time` (
 
 CREATE TABLE `students_sent_message` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`student_check_time_id` int NOT NULL,
-	`status` varchar(20),
+	`emp_id` varchar(10) NOT NULL,
+	`sent_type` varchar(50) NOT NULL,
+	`status` varchar(255),
 	`sent_time` DATETIME NOT NULL,
 	PRIMARY KEY (`id`)
 );
-
-ALTER TABLE `students_sent_message` ADD CONSTRAINT `students_sent_message_fk0` FOREIGN KEY (`student_check_time_id`) REFERENCES `students_check_time`(`id`);
-
